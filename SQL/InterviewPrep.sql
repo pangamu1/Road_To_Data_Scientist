fa77e8d2193729068ef48e3a5adf6fb4c86dc6ff
pangamu1 <31705023+pangamu1@users.noreply.github.com>
2023-07-13 22:28:15 -0500

    Added Interview Prep File

---
+++ SQL/InterviewPrep.sql
@@ -0,0 +1,6 @@
+/* Interview Prep by Data Lemur */
+
+/* Question 1:  Page With No Likes [Facebook SQL Interview Question] - Difficulty Level: Easy
+
+Assume you're given two tables containing data about Facebook Pages and their respective likes (as in "Like a Facebook Page").
+Write a query to return the IDs of the Facebook pages that have zero likes. The output should be sorted in ascending order based on the page IDs. */
