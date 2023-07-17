4a5cec4e3c593664bca069fe0ee294fc3dae5d6b
pangamu1 <31705023+pangamu1@users.noreply.github.com>
2023-07-16 23:55:41 -0500

    Practice SQL

--- SQL/InterviewPrep.sql
+++ SQL/InterviewPrep.sql
@@ -8,5 +8,20 @@ JOIN fpl.stats
 ON players.id = stats.id
 WHERE direct_freekicks_order = 0 AND team = "Arsenal"
 
+-- Question 2: Write a query to count players in Aston Villa and remaining as just others 
+SELECT  
+    COUNT (*) FILTER (WHERE team = 'Aston Villa') AS AstonVilla,
+    COUNT (*) FILTER (WHERE team IN ('Arsenal','Bornemouth','Chelsea')) AS Rest
+FROM 
+    fpl.players
+
+ -- Method #2:
+ SELECT 
+  SUM(CASE WHEN team = 'Aston Villa' THEN 1 ELSE 0 END) AS AstonVilla, 
+  SUM(CASE WHEN team IN ('Arsenal', 'Chelsea') THEN 1 ELSE 0 END) AS Rest 
+FROM fpl.players;
+
+   
+
 
 
